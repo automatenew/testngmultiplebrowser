3a1e6d0c97597c471f6ebaea9db6028a60a87d65
jog <ctfsnesssit@gmail.com>
2019-07-31 23:31:15 -0400

    runner update

--- testngmultiplebrowser/src/test/java/runner/RunnerIT.java
+++ testngmultiplebrowser/src/test/java/runner/RunnerIT.java
@@ -3,9 +3,13 @@ package runner;
 import cucumber.api.CucumberOptions;
 
 @CucumberOptions(glue = "stepdef", 
+                 monochrome = true,
                  plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", 
-                		 "json:target/json-cucumber-reports/cukejson.json","testng:target/testng-cucumber-reports/cuketestng.xml" },
+                		 "json:target/json-cucumber-reports/cukejson.json",
+                		 "testng:target/testng-cucumber-reports/cuketestng.xml",
+                		 "rerun:rerunTC/rerun.txt"},
                  features = "src/test/resources/features/")
+                 //Rerun the feature file by placing "@rerunTC/rerun.txt" or "src/test/resources/features/"
 public class RunnerIT extends AbstractTestNGCucumberParallelTests {
 
 }
